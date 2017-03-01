dc3f85a923cfe1dda80627327d337e0326dc021a
g-lozano <gfourth1005@gmail.com>
2017-03-01 06:01:45 +0000

    update

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import Input from './input.jsx';
-import Table from './table.jsx';
 import ajax from '../utils/ajax.js'
 import axios from 'axios'
 
@@ -14,26 +12,51 @@ class Search extends React.Component {
     }
   }
   handleEnter(e) {
-      var location = document.getElementById('location').value
-      if (e.key == "Enter" && location) {
-        this.setState({
-          search: 'searching...'
-        })
-        var params = '?location='+location
-        ajax('get', '/api/yelp'+params)
-          .then((data) => {
-            //format
-            this.setState({
-                  search: JSON.stringify(data)
-                })
+    var location = document.getElementById('location').value
+    if (e.key == "Enter" && location) {
+      this.setState({
+        search: 'searching...'
+      })
+      var params = '?location=' + location
+      ajax('get', '/api/yelp' + params)
+        .then((data) => {
+          var cafes = []
+          data.businesses.forEach((cafe) => {
+            cafes.push(
+              <div className="cafe-result">
+                    <div className="result-image-div">
+                        <img className="result-image" src={cafe.image_url}/>
+                    </div>
+                    <div className="result-div">
+                      <div className="">
+                          <h4><a href={cafe.url}>{cafe.name}</a></h4>
+                      </div>
+                      <div className="">
+                          {cafe.location.display_address[0]}
+                      </div>
+                    </div>
+              </div>
+            )
           })
-      }
+          
+          var results =  (
+              <div className="results">
+              {cafes}
+              </div>
+            )
+
+          this.setState({
+            search: results
+          })
+        })
+    }
   }
   render() {
     return (
       <div>
         <Input handleEnter={this.handleEnter}/>
-        <Table data={this.state.search}/>
+        <br/>
+        <div>{this.state.search}</div>
       </div>
     )
   }
